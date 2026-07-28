ab102357e488630e6f53e09096d95ff99d8e5466
Ethelyn Matias <ethelyn.matias2015@gmail.com>
2026-07-28 18:13:57 +0800

    Uk search

---
+++ app/Services/SmartSearch/AmlService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services\SmartSearch;
+
+use App\Services\SmartSearch\Exceptions\SmartSearchException;
+
+class AmlService
+{
+    public function __construct(
+        protected SmartSearchClient $client,
+    ) {}
+
+    /**
+     * Run a UK individual AML search.
+     *
+     * @throws SmartSearchException
+     */
+    public function search(array $data): array
+    {
+        return $this->client->post(
+            '/v3/ukindividual/searches',
+            [
+                'data' => [
+                    'type' => 'uk-individual',
+                    'attributes' => [
+                        'name' => [
+                            // Required by the API, not optional.
+                            'title' => $data['title'] ?? null,
+                            'first' => $data['first_name'],
+                            'last' => $data['last_name'],
+                        ],
+
+                        // Plural, and the endpoint only accepts these three subfields.
+                        'addresses' => [
+                            [
+                                'building' => $data['address1'],
+                                'town' => $data['city'],
+                                'postcode' => $data['postcode'],
+                            ],
+                        ],
+
+                        // Optional but recommended
+                        'date_of_birth' => $data['dob'] ?? null,
+                    ],
+                ],
+            ]
+        )->json();
+    }
+}
